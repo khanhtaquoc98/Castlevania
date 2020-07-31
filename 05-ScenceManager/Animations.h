90e5e3ee5064fdd3566ecdbaf1489de7647d461e
khanhtaquoc98 <57973616+khanhtaquoc98@users.noreply.github.com>
2020-07-31 11:55:19 +0700

    Add nx, brick virtual bottom
    nx = 1 //Right, nx = -1 //Left
    Simon on brick bottom

---
+++ 05-ScenceManager/Animations.h
@@ -0,0 +1,79 @@
+﻿#pragma once
+#include <Windows.h>
+#include <d3dx9.h>
+#include <unordered_map>
+
+#include "Sprites.h"
+
+/*
+Sprite animation
+*/
+class CAnimationFrame
+{
+	LPSPRITE sprite;
+	DWORD time;
+
+public:
+	CAnimationFrame(LPSPRITE sprite, int time) { this->sprite = sprite; this->time = time; }
+	DWORD GetTime() { return time; }
+	LPSPRITE GetSprite() { return sprite; }
+};
+
+typedef CAnimationFrame *LPANIMATION_FRAME;
+
+class CAnimation
+{
+	DWORD lastFrameTime;
+	DWORD aniStartTime;   // mốc thời gian kể từ lúc bắt đầu render một animation
+
+	int currentFrame;
+	int defaultTime;
+	vector<LPANIMATION_FRAME> frames;
+public:
+	CAnimation(int defaultTime = 100) { this->defaultTime = defaultTime; lastFrameTime = -1; currentFrame = -1; }
+	void Add(int spriteId, DWORD time = 0);
+
+	void Render(float x, float y,int nx, int alpha = 255);
+
+	void Reset() { currentFrame = -1; }
+	void SetAniStartTime(DWORD t) { aniStartTime = t; }
+	bool IsOver(DWORD dt) { return GetTickCount() - aniStartTime >= dt; };
+};
+
+typedef CAnimation *LPANIMATION;
+
+class CAnimations
+{
+	static CAnimations * __instance;
+
+	unordered_map<int, LPANIMATION> animations;
+
+public:
+	void Add(int id, LPANIMATION ani);
+	LPANIMATION Get(int id);
+	void Clear();
+
+	static CAnimations * GetInstance();
+};
+
+typedef vector<LPANIMATION> CAnimationSet;
+
+typedef CAnimationSet* LPANIMATION_SET;
+
+/*
+	Manage animation set database
+*/
+class CAnimationSets
+{
+	static CAnimationSets * __instance;
+
+	unordered_map<int, LPANIMATION_SET> animation_sets;
+
+public:
+	CAnimationSets();
+	void Add(int id, LPANIMATION_SET ani);
+	LPANIMATION_SET Get(unsigned int id);
+
+
+	static CAnimationSets * GetInstance();
+};
---
+++ 05-ScenceManager/GameObject.h
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <Windows.h>
+#include <d3dx9.h>
+#include <vector>
+
+#include "Sprites.h"
+#include "Animations.h"
+
+
+using namespace std;
+
+#define ID_TEX_BBOX -100		// special texture to draw object bounding box
+
+class CGameObject; 
+typedef CGameObject * LPGAMEOBJECT;
+
+struct CCollisionEvent;
+typedef CCollisionEvent * LPCOLLISIONEVENT;
+struct CCollisionEvent
+{
+	LPGAMEOBJECT obj;
+	float t, nx, ny;
+	
+	float dx, dy;		// *RELATIVE* movement distance between this object and obj
+
+	CCollisionEvent(float t, float nx, float ny, float dx = 0, float dy = 0, LPGAMEOBJECT obj = NULL) 
+	{ 
+		this->t = t; 
+		this->nx = nx; 
+		this->ny = ny;
+		this->dx = dx;
+		this->dy = dy;
+		this->obj = obj; 
+	}
+
+	static bool compare(const LPCOLLISIONEVENT &a, LPCOLLISIONEVENT &b)
+	{
+		return a->t < b->t;
+	}
+};
+
+
+class CGameObject
+{
+public:
+
+	float x; 
+	float y;
+
+	float dx;	// dx = vx*dt
+	float dy;	// dy = vy*dt
+
+	int width;
+	int height;
+
+	float vx;
+	float vy;
+
+	int nx;	 //right = 1; left = -1
+
+	int state;
+
+	DWORD dt; 
+
+	LPANIMATION_SET animation_set;
+
+public: 
+	void SetPosition(float x, float y) { this->x = x, this->y = y; }
+	void GetPosition(float& x, float& y) { x = this->x; y = this->y; }
+
+
+	void SetSpeed(float vx, float vy) { this->vx = vx, this->vy = vy; }
+	void GetSpeed(float &vx, float &vy) { vx = this->vx; vy = this->vy; }
+
+	void SetWidth(int w) { this->width = w; }
+	void GetWidth(int& w) { w = this->width; }
+
+	void SetHeight(int h) { this->height = h; }
+	void GetHeight(int& h) { h = this->height; }
+
+	void SetOrientation(int nx) { this->nx = nx; }
+	int GetOrientation() { return nx; }
+	void ReDirection() { this->nx = -nx; this->vx = -vx; }
+
+
+	int GetState() { return this->state; }
+
+	void RenderBoundingBox();
+
+	void SetAnimationSet(LPANIMATION_SET ani_set) { animation_set = ani_set; }
+
+	LPCOLLISIONEVENT SweptAABBEx(LPGAMEOBJECT coO);
+	void CalcPotentialCollisions(vector<LPGAMEOBJECT> *coObjects, vector<LPCOLLISIONEVENT> &coEvents);
+	void FilterCollision(
+		vector<LPCOLLISIONEVENT> &coEvents, 
+		vector<LPCOLLISIONEVENT> &coEventsResult, 
+		float &min_tx, 
+		float &min_ty, 
+		float &nx, 
+		float &ny, 
+		float &rdx, 
+		float &rdy);
+
+	CGameObject();
+
+	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom) = 0;
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects = NULL);
+	virtual void Render() = 0;
+	virtual void SetState(int state) { this->state = state; }
+
+
+	~CGameObject();
+};
+
